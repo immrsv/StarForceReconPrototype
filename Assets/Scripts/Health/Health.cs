161b4d72af09c8dd4effc1e8d3e23e1764682faf
Jake Perry <jake-perry@live.com>
2017-08-07 00:08:08 +0930

    Health script properties & more less delegate overhead
    Removed obsolete data from the DamageEventDelegate delegate. Previous health & new health can be calculated and obtained via the sender reference, so the data does not need to be passed through.
    Added boolean for alive state.
    Added ApplyHeal function to heal the entity.

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -6,9 +6,9 @@ using UnityEngine;
  * Handles health, damage, healing, etc. */
 public class Health : MonoBehaviour
 {
-    #region Delegates
+    #region Delegates & Events
 
-    public delegate void DamageEventDelegate(object sender, float damageValue, float previousHealth, float newHealth);
+    public delegate void DamageEventDelegate(Health sender, float damageValue);
 
     public event DamageEventDelegate OnDamage;
     public event DamageEventDelegate OnDeath;
@@ -21,16 +21,17 @@ public class Health : MonoBehaviour
 
     [Range(1, 1000), SerializeField]    private int _maxHealth = 100;
     [SerializeField, HideInInspector]   private float _currentHealth = 0.0f;
+    [SerializeField]    private bool _isAlive = true;
 
     #endregion
 
     #region Start Value Variables
 
-    [Range(1, 1000), SerializeField]private int _startingHealth = 100;
+    [Range(1, 1000), SerializeField]    private int _startingHealth = 100;
 
     [SerializeField]    private bool _startRandom = false;
-    [Range(1, 1000), SerializeField]  private int _startMinimum = 60;
-    [Range(1, 1000), SerializeField] private int _startMaximum = 100;
+    [Range(1, 1000), SerializeField]    private int _startMinimum = 60;
+    [Range(1, 1000), SerializeField]    private int _startMaximum = 100;
 
     #endregion
 
@@ -49,51 +50,92 @@ public class Health : MonoBehaviour
 
     #endregion
 
-    void Awake()
-    {
+    #region Get Properties
 
+    public int maxHealth { get { return _maxHealth; } }
+    public bool alive { get { return _isAlive; } }
+
+    public float health
+    {
+        get { return _currentHealth; }
+        set
+        {
+            if (value >= 0)
+            {
+                float oldHealth = _currentHealth;
+                _currentHealth = value;
+
+                if (value == 0)
+                    Death(oldHealth);
+            }
+        }
     }
+
+    #endregion
 
     void Start ()
     {
-		
-	}
-	
-	void Update ()
-    {
-		
-	}
-    
+        /* NOTE: Components will not receive the OnDestroy method call
+         * if they do not have Start, Update, FixedUpdate, etc implemented.
+         * 
+         * The Start function is only implemented here to ensure the OnDestroy
+         * method is called. 
+         */
+    }
+
     /// <summary>
     /// Safely raises the specified event if it has any registered listeners.
     /// </summary>
-    private void RaiseEvent(DamageEventDelegate e, float damageValue, float previousHealth)
+    private void RaiseEvent(DamageEventDelegate e, float damageValue)
     {
         // Check there are any registered listeners before firing event
         DamageEventDelegate del = e;
         if (del != null)
-            e(this, damageValue, previousHealth, _currentHealth);
+            e(this, damageValue);
     }
 
-    private void Death(float damage, float previousHealth)
+    private void Death(float damage)
     {
-        // TODO: Handle death
+        _isAlive = false;
+        _currentHealth = 0;
         
-        RaiseEvent(OnDeath, damage, previousHealth);
+        // Raise event
+        RaiseEvent(OnDeath, damage);
     }
 
+    /// <param name="damage">Damage to apply. Absolute value is used, use ApplyHeal to heal the entity.</param>
     public void ApplyDamage(float damage)
     {
-        float previousHealth = _currentHealth;
+        if (_isAlive)
+        {
+            if (damage < 0) damage *= -1.0f;
 
-        // TODO: Apply damage
+            _currentHealth -= damage;
 
-        RaiseEvent(OnHealthChanged, damage, previousHealth);
-        RaiseEvent(OnDamage, damage, previousHealth);
+            // Raise events
+            RaiseEvent(OnHealthChanged, damage);
+            RaiseEvent(OnDamage, damage);
+        }
+    }
+
+    /// <param name="heal">Healing to apply. Absolute value is used, use ApplyDamage to damage the entity.</param>
+    public void ApplyHeal(float heal)
+    {
+        if (_isAlive)
+        {
+            if (heal < 0) heal *= -1.0f;
+
+            _currentHealth += heal;
+
+            // Raise events
+            RaiseEvent(OnHealthChanged, heal);
+            RaiseEvent(OnHeal, heal);
+        }
     }
 
     void OnDestroy()
     {
-        RaiseEvent(OnDeath, -1, -1);
+        // Call death of script, raising OnDeath event
+        Death(_currentHealth);
     }
 }
