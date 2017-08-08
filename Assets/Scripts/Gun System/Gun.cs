72bf031e3aecadc6e08a000b793404172494b6d0
Jake Perry <jake-perry@live.com>
2017-08-09 00:29:58 +0930

    Gun System & EarlyUpdate
    Created a class to manage EarlyUpdate functionality via delegate.
    Started basic gun system & arsenal script for players to hold guns.

---
+++ Assets/Scripts/Gun System/Gun.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField]    private Transform _gunOrigin = null;
+    private bool _triggerDown = false;
+
+    #region Ammo Variables
+
+    private bool _bottomlessClip = false;
+    private uint _clipSize = 30;
+
+    #endregion
+
+    void Awake()
+    {
+        
+    }
+
+    void Start ()
+    {
+		
+	}
+
+    void FixedUpdate ()
+    {
+        if (_gunOrigin && _triggerDown)
+        {
+            ;
+        }
+	}
+
+    public void TriggerDown()
+    {
+        _triggerDown = true;
+    }
+
+    public void TriggerUp()
+    {
+        _triggerDown = false;
+    }
+}
---
+++ Assets/Scripts/Player Controller/PlayerArsenal.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Handles weapon switching for playable characters. */
+public class PlayerArsenal : MonoBehaviour
+{
+
+    private class GunHolster
+    {
+        public Gun _gun = null;
+        public Transform _holsterPoint = null;
+    }
+
+    [SerializeField]    private GunHolster _primary = null;
+    [SerializeField]    private GunHolster _secondary = null;
+    private GunHolster _current = null;
+    private bool _currentIsPrimary = false;
+
+	void Awake ()
+    {
+        if (_primary != null)
+        {
+            _current = _primary;
+            _currentIsPrimary = true;
+        }
+        else if (_secondary != null)
+        {
+            _current = _secondary;
+            _currentIsPrimary = false;
+        }
+        else
+        {
+            Debug.LogError("No primary or secondary gun specified. Script will be destroyed.", this);
+            Destroy(this);
+            return;
+        }
+
+        // Subscribe EarlyUpdate function
+        EarlyUpdateManager.EarlyUpdate += EarlyUpdate;
+	}
+
+    private void EarlyUpdate()
+    {
+        if (enabled)
+        {
+            if (Input.GetKeyDown("SwitchWeapon"))
+            {
+                // TODO
+            }
+        }
+    }
+}
