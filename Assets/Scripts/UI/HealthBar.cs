0a64c217d9b9bce2dd389e60f27df96f57331a72
Damian <damian.castle@hotmail.com>
2017-08-11 12:22:26 +0930

    Improved health bar script
    Heal bar script now handles death, disabling the bar.
    Now properly scales bar visuals to represent current health when the OnDamage event is called.

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -7,34 +7,33 @@ public class HealthBar : MonoBehaviour
 {
     Health health;
     RectTransform rect;
+    OverheadHUD hud = null;
+    float maxWidth = 0;
 
-    private void _health_OnDamage(Health sender, float damageValue)
+    private void Health_OnDamage(Health sender, float damageValue)
     {
-        // This is called each time the health is changed. 
-        // This is where we update the bar's values
+        rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, (sender.health / sender.maxHealth) * maxWidth);
+    }
 
-        
-
-
-        // TODO:
-        // Use the 'sender' parameter to get the max health
-        // Get the current health
-        // divide current by max and set _barFill to this value
-        // This is now the value to fill the bar
-
-        // Attach a slider UI component or something, and set the value
+    private void Health_OnDeath(Health sender, float damageValue)
+    {
+        gameObject.SetActive(false);
+        if (hud)
+            hud.gameObject.SetActive(false);
     }
 
     void Start ()
     {
         rect = GetComponent<RectTransform>();
-        OverheadHUD hud = transform.parent.GetComponent<OverheadHUD>();
+        maxWidth = rect.sizeDelta.x;
+        hud = transform.parent.GetComponent<OverheadHUD>();
+
         health = hud.target.GetComponent<Health>();
+        if (health)
+        {
+            health.OnDamage += Health_OnDamage;
+            health.OnDeath += Health_OnDeath;
+        }
     }
-
-    void Update()
-    {
-        rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, (100*health.health)/health.maxHealth);
-    }
-
+    
 }
